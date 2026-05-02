6c52d0adce5a9f8fb1fc1a0e6955047aa2e5b90d
SasquatchPunter <jeremyrobertelliott@gmail.com>
2026-05-02 11:49:47 -0600

    init
    refactor to new byoc bring-you-own-client structure

---
+++ packages/speech/README.md
@@ -0,0 +1,3 @@
+# GoogleAPI French Audio Generator
+
+This small tool automates generating french audio translations for large word lists. It's a BYOC (Bring Your Own Client) style of tool that expects a simplified client interface, so you can just wrap your favorite (or self-made) API client and pass it into the generator function.
---
+++ packages/speech/src/lib/config.js
@@ -0,0 +1,34 @@
+/** @typedef {{ file: string; delimiter?: string; }} InputConfig */
+/** @typedef {{ dir?: string; prefix?: string; }} OutputConfig */
+
+/**
+ * Initializes and returns a complete input config.
+ * @param {InputConfig} config
+ * @returns {Required<InputConfig>}
+ */
+function initInputConfig(config) {
+  if (config.file === undefined)
+    throw Error("Missing required `file` configuration option!");
+
+  return {
+    file: config.file,
+    delimiter: config.delimiter ?? "\n",
+  };
+}
+
+/**
+ * Initializes and returns a complete output config.
+ * @param {OutputConfig} config
+ * @returns {Required<OutputConfig>}
+ */
+function initOutputConfig(config) {
+  return {
+    dir: config.dir ?? "./",
+    prefix: config.prefix ?? "",
+  };
+}
+
+module.exports = {
+  initInputConfig,
+  initOutputConfig,
+};
